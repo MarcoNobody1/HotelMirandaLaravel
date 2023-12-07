c32f8a55973828559f5363dae62bc1dd837f76cc
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 18:21:39 +0100

    se crea logica del modelo de booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'surname', 'email', 'phone', 'order_date', 'check_in', 'check_out', 'special_request', 'room_id', 'price', 'status'];
+    protected $table = 'booking';
+    public $timestamps = false;
 }
