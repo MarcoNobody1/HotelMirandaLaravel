8ce91fd8af0c8668ecd44efa8cc1215cdbe8dff0
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 18:20:24 +0100

    se termina controlador de booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,63 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
-    }
+        if (Session::has('arrival') && Session::has('departure') && Session::has('roomId') && Session::has('roomPrice')) {
+            $checkin =  htmlspecialchars(Session::get('arrival'));
+            $checkout =  htmlspecialchars(Session::get('departure'));
+            $roomId = htmlspecialchars(Session::get('roomId'));
+            $roomPrice = htmlspecialchars(Session::get('roomPrice'));
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+        $request->validate([
+            'firstname' => 'required',
+            'surname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'specialrequest' => 'required',
+        ]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        $currentDateTime = now();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $booking = Booking::create([
+            'name' => $request->input('firstname'),
+            'surname' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'order_date' =>  $currentDateTime,
+            'check_in' => $checkin,
+            'check_out' => $checkout,
+            'special_request' => $request->input('specialrequest'),
+            'room_id' => $roomId,
+            'price' => $roomPrice,
+            'status' => 'Check In',
+        ]);
+
+        if ($booking->wasRecentlyCreated) {
+            $notification = [
+                'Thanks! Your booking is done.',
+                'success',
+                false,
+                'Thanks for your time!',
+            ];
+        } else {
+            $notification = [
+                "Oops! Something's wrong. Try again later, please.",
+                'error',
+                false,
+                'Error:',
+            ];
+        }
+
+        Session::forget(['arrival', 'departure']);
+
+        return response()->json(['notification' => $notification]);
     }
 }
