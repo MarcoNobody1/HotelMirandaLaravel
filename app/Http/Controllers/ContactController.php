2dada210e3bc29c1f313355c25bb737d8ae70dd4
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 14:33:32 +0100

    se crea logica del controlador para realizar POST hacia contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -22,6 +23,42 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'subject' => 'required',
+            'message' => 'required',
+        ]);
+
+        $currentDateTime = now();
+        $contact = new Contact();
+        $contact->date = $currentDateTime;
+        $contact->name = htmlspecialchars($request->input('name'));
+        $contact->email = htmlspecialchars($request->input('email'));
+        $contact->phone = htmlspecialchars($request->input('phone'));
+        $contact->subject = htmlspecialchars($request->input('subject'));
+        $contact->comment = htmlspecialchars($request->input('message'));
+        $contact->archived = false;
+
+        $contact->save();
+
+        if ($contact->wasRecentlyCreated) {
+            $notification = [
+                'Thanks! Your message will be reviewed soon.',
+                'success',
+                false,
+                'Thanks for your time!',
+            ];
+        } else {
+            $notification = [
+                "Oops! Something's wrong. Try again later, please.",
+                'error',
+                false,
+                'Error:',
+            ];
+        }
+
+        return response()->json(['notification' => $notification]);
     }
 }
