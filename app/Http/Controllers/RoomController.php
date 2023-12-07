ffd6e2af9ef28ec2e1b483c92e29a13f7eadc613
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 14:34:32 +0100

    se separa la logica para obtener todas las habitaciones o solo las habitaciones buscadas

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Room;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class RoomController extends Controller
@@ -15,43 +13,17 @@ class RoomController extends Controller
      */
     public function index()
     {
-        if (request()->has('arrival') && request()->has('departure')) {
-            $checkin = htmlspecialchars(request()->input('arrival'));
-            Session::put('arrival', $checkin);
-            $checkout = htmlspecialchars(request()->input('departure'));
-            Session::put('departure', $checkout);
 
-            $rooms = Room::select('room.*')
+        $rooms = Room::select('room.*')
             ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
             ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
             ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
             ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
             ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
             ->where('room.availability', 'Available')
-            ->whereNotExists(function (Builder $subquery) use ($checkin, $checkout) {
-                $subquery->selectRaw(1)
-                    ->from('booking')
-                    ->whereColumn('room.id', 'booking.room_id')
-                    ->where(function (Builder $query) use ($checkin, $checkout) {
-                        $query->whereBetween(DB::raw("'$checkin'"), ['booking.check_in', 'booking.check_out'])
-                            ->orWhereBetween(DB::raw("'$checkout'"), ['booking.check_in', 'booking.check_out'])
-                            ->orWhereBetween('booking.check_in', [DB::raw("'$checkin'"), DB::raw("'$checkout'")])
-                            ->orWhereBetween('booking.check_out', [DB::raw("'$checkin'"), DB::raw("'$checkout'")]);
-                    });
-            })
-                ->groupBy('room.id')
-                ->get();
-        } else {
-            $rooms = Room::select('room.*')
-                ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
-                ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
-                ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
-                ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
-                ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
-                ->where('room.availability', 'Available')
-                ->groupBy('room.id')
-                ->get();
-        }
+            ->groupBy('room.id')
+            ->get();
+
         $roomsArray = $rooms->toArray();
 
         foreach ($roomsArray as &$room) {
@@ -77,7 +49,21 @@ class RoomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $checkin = htmlspecialchars($request->query('arrival'));
+        $checkout = htmlspecialchars($request->query('departure'));
+        Session::put('arrival', $checkin);
+        Session::put('departure', $checkout);
+        $roominstance = new Room();
+        $rooms = $roominstance->getRooms($checkin, $checkout);
+        $roomsArray = $rooms->toArray();
+
+        foreach ($roomsArray as &$room) {
+            $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
+        }
+
+        $chunks = array_chunk($roomsArray, 5);
+
+        return view('rooms', ['rooms' => $chunks]);
     }
 
     /**
@@ -85,6 +71,44 @@ class RoomController extends Controller
      */
     public function show(string $id)
     {
-        //
+        if (Session::has('arrival') && Session::has('departure')) {
+            $checkin =  htmlspecialchars(Session::get('arrival'));
+            $checkout =  htmlspecialchars(Session::get('departure'));
+        }
+
+        $roomraw = Room::select('room.*')
+            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
+            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
+            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
+            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
+            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
+            ->where('room.id', $id)
+            ->groupBy('room.id')
+            ->first();
+
+        $roomDetails = $roomraw->toArray();
+
+        $roomDetails['finalPrice'] = $roomDetails['price'] - ($roomDetails['price'] * ($roomDetails['discount'] / 100));
+
+        $roomsraw = Room::select('room.*')
+            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
+            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
+            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
+            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
+            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
+            ->where('room.availability', 'Available')
+            ->where('room.discount', '!=', 0)
+            ->groupBy('room.id')
+            ->get();
+
+        $roomsArray = $roomsraw->toArray();
+
+        $randomRoomIndices = array_rand($roomsArray, 5);
+        $recommendedRooms = array_intersect_key($roomsArray, array_flip($randomRoomIndices));
+        foreach ($recommendedRooms as &$room) {
+            $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
+        }
+
+        return view('roomdetails', ['roomdetails' => $roomDetails, 'recommendedRooms' => $recommendedRooms, 'checkin' => $checkin, 'checkout' => $checkout]);
     }
 }
