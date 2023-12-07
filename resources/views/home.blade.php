927c7b1bcb99cfe088a94ba41feaf4f8188aa90d
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 14:35:16 +0100

    se modifica ruta hacia la que mandar los datos de la busqueda

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@ Hotel Miranda | Home
         <button type="button" onclick="goToAboutUs()" class="intro__info-learnbtn">LEARN MORE</button>
     </div>
 
-    <form name="dates-form" class="intro__form" id="checkavailability_form" action="/rooms" method="GET">
+    <form name="dates-form" class="intro__form" id="checkavailability_form" action="/rooms-search" method="GET">
         <label for="arrival" class="intro__form-arrivallbl">Arrival Date</label>
         <input name="arrival" type="date" value="{{date('Y-m-d')}}" min="{{date('Y-m-d')}}" max="{{date('Y-m-d', strtotime('+1 Year'))}}" id="arrival" class="intro__form-arrivalinp">
         <label for="departure" class="intro__form-departurelbl">Departure Date</label>
