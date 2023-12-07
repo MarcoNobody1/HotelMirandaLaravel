e872ed08ff5c10cb44d61cd4a80165d287e0cc8d
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 23:12:02 +0100

    se crea funcion de obtencion de ofertas desde el buscador de offers

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -40,4 +40,33 @@ class Room extends Model
 
         return $rooms;
     }
+
+    public function getOffers($checkin, $checkout)
+    {
+
+
+        $rooms = Room::select('room.*')
+            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
+            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
+            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
+            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
+            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
+            ->where('room.availability', 'Available')
+            ->where('room.discount', '!=', 0)
+            ->whereNotExists(function (Builder $subquery) use ($checkin, $checkout) {
+                $subquery->selectRaw(1)
+                    ->from('booking')
+                    ->whereColumn('room.id', 'booking.room_id')
+                    ->where(function (Builder $query) use ($checkin, $checkout) {
+                        $query->whereBetween(DB::raw("'$checkin'"), ['booking.check_in', 'booking.check_out'])
+                            ->orWhereBetween(DB::raw("'$checkout'"), ['booking.check_in', 'booking.check_out'])
+                            ->orWhereBetween('booking.check_in', [DB::raw("'$checkin'"), DB::raw("'$checkout'")])
+                            ->orWhereBetween('booking.check_out', [DB::raw("'$checkin'"), DB::raw("'$checkout'")]);
+                    });
+            })
+            ->groupBy('room.id')
+            ->get();
+
+        return $rooms;
+    }
 }
