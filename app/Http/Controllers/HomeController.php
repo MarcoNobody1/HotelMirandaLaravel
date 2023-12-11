f4fedcfd280e70fe86d8abca47304a56b76a41b9
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-11 18:38:04 +0100

    se actualizan queries para home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,13 +18,7 @@ class HomeController extends Controller
             Session::forget(['roomId', 'roomPrice']);
         }
 
-        $rooms = Room::select('room.*')
-            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
-            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
-            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
-            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
-            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
-            ->where('room.availability', 'Available')
+        $rooms = Room::where('room.availability', 'Available')
             ->groupBy('room.id')
             ->get();
 
@@ -33,53 +27,5 @@ class HomeController extends Controller
         }
 
         return view('home', ['rooms' => $rooms]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
