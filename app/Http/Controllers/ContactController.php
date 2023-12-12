c8caeba21548cea9bd30a55846105dcf997c58b2
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-12 22:03:53 +0100

    se eliminan las variables de sesion

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
      */
     public function create()
     {
-        Session::forget(['arrival', 'departure']);
+     
         return view('contact');
     }
 
