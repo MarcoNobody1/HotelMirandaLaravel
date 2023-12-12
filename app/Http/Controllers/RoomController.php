0c864356b9df4b867bedb2a2d61e8f7798a2d9bd
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-12 17:56:10 +0100

    se simplifica la logica de los metodos de rooms

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -8,38 +8,14 @@ use Illuminate\Support\Facades\Session;
 
 class RoomController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $checkin = '';
         $checkout = '';
 
-        // if (Session::has('arrival') && Session::has('departure')) {
-        //     $checkin =  htmlspecialchars(Session::get('arrival'));
-        //     $checkout =  htmlspecialchars(Session::get('departure'));
-        //     $roominstance = new Room();
-        //     $rooms = $roominstance->getRooms($checkin, $checkout);
-        // } else {
-        //     $rooms = Room::select('room.*')
-        //         ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
-        //         ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
-        //         ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
-        //         ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
-        //         ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
-        //         ->where('room.availability', 'Available')
-        //         ->groupBy('room.id')
-        //         ->get();
-        // }
-
         $rooms = Room::all();
 
-        foreach ($rooms as $room) {
-            foreach ($room->getPhotos as $photo) {
-                $photourl = $photo;
-            }
-        }
+        $photoArray = Room::organizePhotos($rooms);
 
         $roomsArray = $rooms->toArray();
 
@@ -49,29 +25,18 @@ class RoomController extends Controller
 
         $chunks = array_chunk($roomsArray, 5);
 
-        return view('rooms', ['rooms' => $chunks, 'checkin' => $checkin, 'checkout' => $checkout, 'photo' => $photourl]);
+        return view('rooms', ['rooms' => $chunks, 'arrival' => $checkin, 'departure' => $checkout, 'photo' => $photoArray]);
     }
 
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function search(Request $request)
     {
         $checkin = htmlspecialchars($request->query('arrival'));
         $checkout = htmlspecialchars($request->query('departure'));
-        Session::put('arrival', $checkin);
-        Session::put('departure', $checkout);
-        $roominstance = new Room();
-        $rooms = $roominstance->getRooms($checkin, $checkout);
+
+        $rooms = Room::getRooms($checkin, $checkout);
+
+        $photoArray = Room::organizePhotos($rooms);
+
         $roomsArray = $rooms->toArray();
 
         foreach ($roomsArray as &$room) {
@@ -80,58 +45,34 @@ class RoomController extends Controller
 
         $chunks = array_chunk($roomsArray, 5);
 
-        return view('rooms', ['rooms' => $chunks, 'checkin' => $checkin, 'checkout' => $checkout]);
+        return view('rooms', ['rooms' => $chunks, 'arrival' => $checkin, 'departure' => $checkout, 'photo' => $photoArray]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(string $id, Request $request)
     {
-        if (Session::has('arrival') && Session::has('departure')) {
-            $checkin =  htmlspecialchars(Session::get('arrival'));
-            $checkout =  htmlspecialchars(Session::get('departure'));
+        $checkin = htmlspecialchars($request->query('arrival'));
+        $checkout = htmlspecialchars($request->query('departure'));
+
+        $roomDetails = Room::findOrFail($id);
+
+        $roomId = $roomDetails->id;
+        $roomPrice = $roomDetails->price;
+
+
+        $roomDetails->finalPrice = $roomDetails->price - ($roomDetails->price * ($roomDetails->discount / 100));
+
+        if ($checkin && $checkout) {
+            $recommendedRooms = Room::getRooms($checkin, $checkout)->shuffle()->take(5);
+        } else {
+            $recommendedRooms = Room::all()->shuffle()->take(5);
         }
 
-        $roomraw = Room::select('room.*')
-            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
-            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
-            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
-            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
-            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
-            ->where('room.id', $id)
-            ->groupBy('room.id')
-            ->first();
 
-        $roomDetails = $roomraw->toArray();
 
-        $roomId = $roomDetails['id'];
-        $roomPrice = $roomDetails['price'];
-
-        Session::put('roomId', $roomId);
-        Session::put('roomPrice', $roomPrice);
-
-        $roomDetails['finalPrice'] = $roomDetails['price'] - ($roomDetails['price'] * ($roomDetails['discount'] / 100));
-
-        $roomsraw = Room::select('room.*')
-            ->selectRaw('GROUP_CONCAT(DISTINCT photos.photo_url) as photo')
-            ->selectRaw('GROUP_CONCAT(amenity.amenity) as amenity')
-            ->leftJoin('photos', 'room.id', '=', 'photos.room_id')
-            ->leftJoin('room_amenities', 'room.id', '=', 'room_amenities.room_id')
-            ->leftJoin('amenity', 'room_amenities.amenity_id', '=', 'amenity.id')
-            ->where('room.availability', 'Available')
-            ->where('room.discount', '!=', 0)
-            ->groupBy('room.id')
-            ->get();
-
-        $roomsArray = $roomsraw->toArray();
-
-        $randomRoomIndices = array_rand($roomsArray, 5);
-        $recommendedRooms = array_intersect_key($roomsArray, array_flip($randomRoomIndices));
         foreach ($recommendedRooms as &$room) {
             $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
         }
 
-        return view('roomdetails', ['roomdetails' => $roomDetails, 'recommendedRooms' => $recommendedRooms, 'checkin' => $checkin, 'checkout' => $checkout]);
+        return view('roomdetails', ['roomdetails' => $roomDetails, 'recommendedRooms' => $recommendedRooms, 'checkin' => $checkin, 'checkout' => $checkout, 'roomId' => $roomId, 'roomPrice' => $roomPrice]);
     }
 }
