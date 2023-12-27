1d1ba8567a5218fbcd86c9c0a1c05885c306a1ce
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-27 09:54:48 +0100

    se crea alias para home

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/aboutus', [AboutusController::class, 'index']);
 
